9adb10ff42854dc7d17453429352a00778f757b3
MDSAJIBCHOWDHURY <mdsajibcse14@gmail.com>
2021-09-06 19:46:37 -0700

    day3 completed

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:first_app/homepage.dart';
+import 'package:first_app/pages/homepage.dart';
+import 'package:first_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+        ),
+        darkTheme: ThemeData(
+          primaryColor: Colors.blue,
+        ),
+        initialRoute: '/home',
+        routes: {
+          '/': (context) => LoginPage(),
+          '/login': (context) => LoginPage(),
+          '/home': (context)=> HomePage(),
+        });
   }
 }
